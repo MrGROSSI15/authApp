c0cf2ead52186ca8d28a29fa6d542987a52de171
Marc Grossi <tecnogrossi@gmail.com>
2024-06-11 00:39:19 +0200

    finish class 363 section 26

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { environment } from '../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>();
+  constructor() { }
+
+  login( email:string, password:string): Observable<boolean> {
+    return of(true)
+  }
+}
